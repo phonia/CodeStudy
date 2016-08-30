9533d2f3874d4326f9fd167619782bd0823abbea
hy <cengling17@hotmail.com>
2016-08-30 17:08:42 +0800

    hy <cengling17@hotmail.com>

---
+++ MultiLanguageRes/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MultiLanguageRes.Models;
+
+namespace MultiLanguageRes.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            try
+            {
+                CultureInfo.CurrentCulture.Name=
+                var pp = new Product();
+                return View(pp);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ MultiLanguageRes/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using MultiLanguageRes.Resources;
+
+namespace MultiLanguageRes.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessageResourceType = typeof(Lang), ErrorMessageResourceName = "Name")]
+        public String Name { get; set; }
+
+        [Required(ErrorMessageResourceType=typeof(Lang),ErrorMessageResourceName="Amount")]
+        public int Amount { get; set; }
+    }
+}
